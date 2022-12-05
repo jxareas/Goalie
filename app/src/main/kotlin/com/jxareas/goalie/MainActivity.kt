ff68c973528fd25478f3688b9975cedac35ef1f2
jxareas <areasjx@gmail.com>
2022-12-05 01:31:01 -0600

    Add Base Application Theme

--- app/src/main/kotlin/com/jxareas/goalie/MainActivity.kt
+++ app/src/main/kotlin/com/jxareas/goalie/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.jxareas.goalie
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateOvershootInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.splashscreen.SplashScreenViewProvider
+import com.jxareas.goalie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+
 }
